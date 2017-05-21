9483da6c0466c09a0993985265cbf22f1f47add0
dppedersen <pedersen.duncan@gmail.com>
2017-05-21 17:15:51 -0400

    Initialize Initial User Fetch On App Load

--- App/Src/Actions/UserActions.js
+++ App/Src/Actions/UserActions.js
@@ -17,3 +17,25 @@ export const fetchUserFail = () => {
     response: 'Error Fetching User'
   }
 };
+
+export const fetchUser = () => {
+  return (dispatch) => {
+    //Start loading animation
+    dispatch(fetchUserInit());
+    console.log('just dispatched fetchuserinit');
+    //Begin fetching
+    return fetch('http://192.168.1.5:8080/api/users', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(data => {
+      data.json().then(data => dispatch(fetchUserSuccess(data)));
+    })
+    .catch(() => {
+      dispatch(fetchUserFail());
+    });
+  }
+}
--- App/Src/Reducers/FetchUser.js
+++ App/Src/Reducers/FetchUser.js
@@ -1,14 +1,24 @@
-const user = (state = {}, action) => {
+const user = (state = {
+  isFetching: false,
+  userData: {}
+}, action) => {
   switch (action.type) {
     case ('FETCH_USER_INIT'):
-
+      return Object.assign({}, state, {
+        isFetching: true
+      });
     case ('FETCH_USER_SUCCESS'):
-
+      return Object.assign({}, state, {
+        isFetching: false,
+        userData: action.response
+      });
     case ('FETCH_USER_FAIL'):
-
+      return Object.assign({}, state, {
+        isFetching: false
+      });
     default:
       return state;
   }
 };
 
-export default photoCount;
+export default user;
