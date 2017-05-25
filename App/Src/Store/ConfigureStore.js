2e720eeec3ad9c37e4e13314e8254cb93c44807e
dppedersen <pedersen.duncan@gmail.com>
2017-05-25 17:39:19 -0400

    Uncomment Store Persistence

--- App/Src/Store/ConfigureStore.js
+++ App/Src/Store/ConfigureStore.js
@@ -35,6 +35,6 @@ export default (initialState) => {
     )
   )
   // https://github.com/rt2zz/redux-persist
-  // persistStore(store, {storage: AsyncStorage})
+  persistStore(store, {storage: AsyncStorage})
   return store
 }
