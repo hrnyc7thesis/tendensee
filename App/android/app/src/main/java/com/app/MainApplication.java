8f0551ec795044ad972f528f2a34ae60a716d924
Debasish Mozumder <debasishbd@hotmail.com>
2017-06-01 12:14:23 -0400

    conflict on merge

--- App/android/app/src/main/java/com/app/MainApplication.java
+++ App/android/app/src/main/java/com/app/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
+import com.azendoo.reactnativesnackbar.SnackbarPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new SvgPackage(),
+            new SnackbarPackage(),
+            new ImagePickerPackage(),
             new RCTCameraPackage(),
             new VectorIconsPackage()
       );
