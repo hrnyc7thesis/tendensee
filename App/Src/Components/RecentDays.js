9cca0b8059b3dd891dfb163f88b8443bd1539253
palante <ppalantee@gmail.com>
2017-06-01 14:19:15 -0400

    Sort habit days by most recent on left

--- App/Src/Components/RecentDays.js
+++ App/Src/Components/RecentDays.js
@@ -2,13 +2,15 @@ import React, { PropTypes } from 'react'
 import { StyleSheet, ScrollView, Text } from 'react-native';
 import Day from './Day.js';
 
-const RecentDays = ({dates, habitProps, onPressItem}) => (
-  <ScrollView horizontal style={styles.recentDaysContainer}>
-    {dates.length ? dates.map((day) => {
-      return <Day key={day.id} day={day} habitProps={habitProps} onPressItem={onPressItem} />
-    }) : <Text>''</Text>}
-  </ScrollView>
-)
+const RecentDays = ({dates, habitProps, onPressItem}) => {
+  return(
+    <ScrollView horizontal style={styles.recentDaysContainer}>
+      {dates.length ? dates.sort((a,b) => new Date(b.date) - new Date(a.date)).map((day) => {
+        return <Day key={day.id} day={day} habitProps={habitProps} onPressItem={onPressItem} />
+      }) : <Text>''</Text>}
+    </ScrollView>
+  )
+}
 
 const styles = StyleSheet.create({
   recentDaysContainer: {
