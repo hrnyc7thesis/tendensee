af4946c0fb84a54c3d9c193e9c01765f1433b49a
palante <ppalantee@gmail.com>
2017-06-02 11:38:15 -0400

    Categorize habits by alreadyCompleted, toComplete

--- App/Src/Components/HabitsList.js
+++ App/Src/Components/HabitsList.js
@@ -2,17 +2,58 @@ import React, { PropTypes } from 'react'
 import { ScrollView, View, Text } from 'react-native';
 import HabitBlock from './HabitBlock.js'
 import ModalRoot from './ModalRoot.js'
+var moment = require('moment');
 
 const HabitsList = ({habits, test, onPressHabit}) => {
+  let toComplete = [];
+  let alreadyCompleted = [];
+
+  habits.forEach(habit => {
+    let toCategorize = habit.dates.sort((a,b) => new Date(b.date) - new Date(a.date))
+    // let mostRecentDayIndex = toCategorize.dates.length - 2;
+    let mostRecentDay;
+    console.log('<<<', toCategorize);
+
+    mostRecentDay = toCategorize[1]
+    console.log('###', mostRecentDay)
+    if (mostRecentDay) {
+      let recentDay = moment(mostRecentDay.date).format('YYYY-MM-DD');
+      let toMatch = moment().subtract(2, 'days').format('YYYY-MM-DD');
+
+      console.log('>>>', recentDay, toMatch);
+      if (recentDay === toMatch) {
+        alreadyCompleted.push(habit);
+      } else {
+        toComplete.push(habit);
+      }
+    }
+
+  })
+
+  console.log('!!!', alreadyCompleted)
 
   return(
     <ScrollView>
-      {habits ? habits.map(habit => {
+      <Text>alreadyCompleted</Text>
+      { alreadyCompleted.map(habit => {
         return <HabitBlock key={habit.id} allHabits={habits} habit={habit} onPressHabit={onPressHabit} onPressItem={test}/>
-      }):<Text>''</Text>}
+      })}
+      <Text>toComplete</Text>
+      { toComplete.map(habit => {
+        return <HabitBlock key={habit.id} allHabits={habits} habit={habit} onPressHabit={onPressHabit} onPressItem={test}/>
+      })}
       <ModalRoot />
     </ScrollView>
   )
+
+  // return(
+  //   <ScrollView>
+  //     {habits ? habits.map(habit => {
+  //       return <HabitBlock key={habit.id} allHabits={habits} habit={habit} onPressHabit={onPressHabit} onPressItem={test}/>
+  //     }):<Text>''</Text>}
+  //     <ModalRoot />
+  //   </ScrollView>
+  // )
 }
 
 export default HabitsList
