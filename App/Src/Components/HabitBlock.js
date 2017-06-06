93b29b1eb33bbe381de6efa9d178ccd0e52a348a
palante <ppalantee@gmail.com>
2017-06-06 16:26:43 -0400

    Style habit block titles

--- App/Src/Components/HabitBlock.js
+++ App/Src/Components/HabitBlock.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react'
 import { Text, View, StyleSheet, Dimensions } from 'react-native';
-import RecentDays from './RecentDays.js'
-
+import RecentDays from './RecentDays.js';
+import { Button, Icon, H1 } from 'native-base';
 const HabitBlock = ({habit, allHabits, onPressPic, onPressNoPic, onPressHabit}) => {
   const habitProps = habit;
 
@@ -9,6 +9,7 @@ const HabitBlock = ({habit, allHabits, onPressPic, onPressNoPic, onPressHabit}) 
     <View style={styles.habitBlock}>
       <View style={styles.habitName}>
         <Text onPress={() => onPressHabit(habitProps)} style={styles.habitNameTitle}>{habit.name}</Text>
+        <Button onPress={() => onPressHabit(habitProps)} style={{paddingLeft: 0, paddingRight: 0, height: 30}}transparent iconLeft><Icon name='flash' /></Button>
       </View>
       <RecentDays allHabits={allHabits} dates={habit.dates} habitProps={habitProps} onPressNoPic={onPressNoPic} onPressPic={onPressPic}/>
     </View>
@@ -19,20 +20,24 @@ const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   habitBlock: {
     flexDirection: 'column',
-    alignItems: 'center',
+    // alignItems: 'center',
     backgroundColor: 'white',
-    marginBottom: 10,
+    marginBottom: 0,
   },
 
   habitName: {
+    flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'salmon',
     width: width,
-    height: 20,
+    height: 30,
   },
 
   habitNameTitle: {
     color: 'white',
+    // fontWeight: 'bold',
+    // fontSize: 20
   },
 })
 
