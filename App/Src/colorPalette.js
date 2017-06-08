03eaa7cdd17d6bb9fbcc1875ad785ce727cbc9a7
Tommy O'Connor <tgoc99@gmail.com>
2017-06-08 19:30:10 -0400

    ...

--- App/Src/colorPalette.js
+++ App/Src/colorPalette.js
@@ -1,9 +1,23 @@
 export default colors = {
-  primary: '#b71c1c',
-  primaryLight: '#f05545',
-  primaryDark: '#7f0000',
-  secondary: '#263238',
-  secondaryLight: '#4f5b62',
-  secondaryDark: '#000a12',
-  text: '#ffffff',
-};
+  primary: '#0277bd',
+  primaryLight: '#58a5f0',
+  primaryDark: '#004c8c',
+  secondary: '#cfd8dc',
+  secondaryLight: '#ffffff',
+  secondaryDark: '#9ea7aa',
+  primaryText: '#ffffff',
+  secondaryText: '#000000',
+  background: '#f5f5f6'
+}
+
+// export default colors = {
+//   primary: '#b71c1c',
+//   primaryLight: '#f05545',
+//   primaryDark: '#7f0000',
+//   secondary: '#a7ffeb',
+//   secondaryLight: '#dbffff',
+//   secondaryDark: '#75ccb9',
+//   primaryText: '#ffffff',
+//   secondaryText: '#000000',
+//   background: '#f5f5f6',
+// }
