06cb274f135ceb48bdd264e119313cf5897cc852
dppedersen <pedersen.duncan@gmail.com>
2017-06-08 19:19:15 -0400

    Add Colors

---
+++ App/Src/ColorPalette.js
@@ -0,0 +1,11 @@
+export default colors = {
+  primary: '#0277bd',
+  primaryLight: '#58a5f0',
+  primaryDark: '#004c8c',
+  secondary: '#cfd8dc',
+  secondaryLight: '#ffffff',
+  secondaryDark: '#9ea7aa',
+  primaryText: '#ffffff',
+  secondaryText: '#000000',
+  background: '#f5f5f6'
+}
